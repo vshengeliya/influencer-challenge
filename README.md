252ebde9eb8fb3f5d6ef5b39ba7eed0d070143e3
Veronika Dodda <v.shengeliya@gmail.com>
2020-12-23 11:36:53 -0500

    final

--- README.md
+++ README.md
@@ -10,4 +10,14 @@ at `http://localhost:3000/users`.
 To open the App use command in another tab:
  * `open index.html`.
 
- 
+ For testing purposes:
+
+ 1. Subscribe a new user - use any email;
+ 2. Subscribe a referred user - use any email and only valid code(aka DEF, HELLO, etc.)
+ 3. Get a referral count for a particular User. - use valid email (example1@google.com, example2@google.com etc.)
+ 4. Who has referred this email - user an email from emailedReffered list (example-reffered1@google.com, example-reffered2@google.com, etc.)
+ 5. Who has a particular user referred - use valid email (example1@google.com, example2@google.com etc.)
+
+ Feel free to reach out with any questions.
+
+ Created by Veronika Dodda.
--- index.js
+++ index.js
@@ -1,26 +1,116 @@
 
 const BASE_URL = "http://localhost:3000/users"
+let refCode = Math.random().toString(20).substring(2, 5) + Math.random().toString(20).substring(2, 5)
+
+function containerAddUser(){
+    let containerAddUser = document.getElementById('container-add-user')
+    containerAddUser.innerHTML=`
+    <form id="add-user">
+              <h3>Subscribe a new user
+      
+              <input
+                type="text"
+                name="email"
+                placeholder="add user email"
+                class="input-text"
+              />
+              
+              <input
+                type="submit"
+                name="submit"
+                value="Subscribe"
+                class="submit"
+              />
+            </h3>
+    </form>
+    `
+}
+
+containerAddUser()
 
 function addUser(){
 
-    const form = document.getElementById('add-user')
+    let formAddUser = document.getElementById('add-user')
+
      document.addEventListener("submit", function(e){
     
+        e.preventDefault()
         let email = e.target.email.value
-        let refCode = Math.random().toString(20).substring(2, 5) + Math.random().toString(20).substring(2, 5)
-         
-         e.preventDefault()
+        let refCode = Math.random().toString(20).substring(2, 5) + Math.random().toString(20).substring(2, 5) 
     
-        form.remove()
-        let container = document.getElementById('container')
         let h3 = document.createElement('h3')
-        container.appendChild(h3)
+        formAddUser.appendChild(h3)
     
         h3.innerText = `Subscribed ${email}, their refferral code is ${refCode}`
      })
 }
 
 addUser()
+
+
+function containerReferredUser(){
+    let containerAddUser = document.getElementById('container-referred-user')
+    containerAddUser.innerHTML=`
+    <br
+    <br/>
+    <form id="add-referred-user">
+        <h3>Subscribe a referred user
+
+        <input
+          type="text"
+          name="emailNew"
+          placeholder="add user email"
+          class="input-text"
+        />
+        <input
+          type="text"
+          name="code"
+          placeholder="referred by"
+          class="input-text"
+        />
+        
+        <input
+          type="submit"
+          name="submit"
+          value="Subscribe"
+          class="submit"
+        />
+      </h3>
+      </form>
+      <br/>
+      <br/>
+    `
+}
+
+containerReferredUser()
+
+
+function addRerreredUser(){
+
+    let formRefferedUser = document.getElementById('add-referred-user')
+     document.addEventListener("submit", function(e){
+    
+        e.preventDefault()
+        let email = e.target.emailNew.value
+        let refCode = Math.random().toString(20).substring(2, 5) + Math.random().toString(20).substring(2, 5)
+
+        let code = e.target.code.value
+        let h3 = document.createElement('h3')
+        formRefferedUser.appendChild(h3)
+
+        fetch(BASE_URL)
+        .then(resp => resp.json())
+        .then( user => { user.forEach(user => {
+            
+            if (user.code === code){
+                return h3.innerText = `Subscribed ${email}, their refferral code is ${refCode}`
+            } 
+         })    
+        })     
+     })
+}
+
+addRerreredUser()
 
 function showPanel(){
 
@@ -29,8 +119,9 @@ function showPanel(){
     
     <h3>
     <form id='count-email'>
-    <label >Get refferal count for a particular User. Enter a user email:</label>
-    <input type="text" id="count" name="count">
+    <label >Get refferal count for a particular User.</label>
+    <input type="text" id="count" name="count" placeholder="enter a user email">
+    
     <input type="submit" value="Submit">
     </form>
     </h3>
@@ -38,8 +129,8 @@ function showPanel(){
     <br/>
     <h3>
     <form id='who-reffered-email'>
-    <label for="fname">Who has reffered this email. Enter an email:</label>
-    <input type="text" id="reffered" name="reffered">
+    <label for="fname">Who has reffered this email.</label>
+    <input type="text" id="reffered" name="reffered" placeholder="enter a user email">
     <input type="submit" value="Submit">
     </form>
     </h3>
@@ -47,8 +138,8 @@ function showPanel(){
     <br/>
     <h3>
     <form id='all-reffered-email'>
-    <label>Who has a particular user reffered. Enter a user email:</label>
-    <input type="text" id="all" name="all">
+    <label>Who has a particular user reffered.</label>
+    <input type="text" id="all" name="all" placeholder="enter a user email">
     <input type="submit" value="Submit">
     </form>
     </h3>
@@ -74,8 +165,8 @@ function emailCount(){
             if (user.email === email){
                 return  formCountEmail.innerHTML =`
                 <form id='count-email'>
-                <label >Get refferal count for a particular User. Enter a user email:</label>
-                <input type="text" id="count" name="count">
+                <label >Get refferal count for a particular User.</label>
+                <input type="text" id="count" name="count" placeholder="enter a user email">
                 <input type="submit" value="Submit">
                 Count: ${user.emailedReffered.length}
                 </form>
@@ -103,8 +194,8 @@ function allEmails(){
             if (user.email === email){
                 return  formAllEmails.innerHTML =`
                 <form id='count-email'>
-                <label >Check how many refferals this email has. Enter en email:</label>
-                <input type="text" id="count" name="count">
+                <label >Check how many refferals this email has.</label>
+                <input type="text" id="count" name="count" placeholder="enter a user email">
                 <input type="submit" value="Submit">
                 Refferals: ${user.emailedReffered}
                 </form>
@@ -120,7 +211,6 @@ allEmails()
 function whoReffered(){
 
     let formWhoReferred = document.querySelector('#who-reffered-email')
-    console.log(formWhoReferred)
     formWhoReferred.addEventListener("submit", function(e){
 
     
@@ -135,10 +225,10 @@ function whoReffered(){
                     if (user.emailedReffered.includes(email)){
                         return formWhoReferred.innerHTML =`
                         <form id='who-reffered-email'>
-                        <label for="fname">Who has reffered this email. Enter an email:</label>
-                        <input type="text" id="reffered" name="reffered">
+                        <label for="fname">Who has reffered this email.</label>
+                        <input type="text" id="reffered" name="reffered" placeholder="enter a user email">
                         <input type="submit" value="Submit">
-                        Reffered: ${user.email}
+                        Reffered by: ${user.email}
                         </form>
                         `
                     }
